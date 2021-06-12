8202cb9a1cf4b99fa37acd14567435d346281226
Fabian Cook <hello@fabiancook.dev>
2021-06-12 22:09:53 +1200

    Add thanks slide

--- src/contents/slides/closing/index.ts
+++ src/contents/slides/closing/index.ts
@@ -1,3 +1,4 @@
 export * from "./links";
 export * from "./licence";
 export * from "./contribute";
+export * from "./thanks";
---
+++ src/contents/slides/closing/thanks.tsx
@@ -0,0 +1,8 @@
+import { h } from "../../../h";
+import { Slide, SlideTokenVNode } from '../slide';
+
+export const ClosingThanks = (
+  <Slide index={9004} id="closing-thanks">
+    <h1>Thanks</h1>
+  </Slide>
+)
