f79ad974d7090d0bebb075c9058c10a04197d706
Fabian Cook <hello@fabiancook.dev>
2021-06-18 00:32:51 +1200

    Add additional deno example

--- src/contents/slides/additional-points/deno.tsx
+++ src/contents/slides/additional-points/deno.tsx
@@ -7,6 +7,13 @@ export const ProduceDeno = (
       {`
 deno run \\
   --import-map=https://cdn.skypack.dev/@virtualstate/deno/import-map.json \\
+  https://cdn.skypack.dev/@virtualstate/deno/lib/example-no-net.js
+`.trim()}
+    </pre>
+    <pre>
+      {`
+deno run \\
+  --import-map=https://cdn.skypack.dev/@virtualstate/deno/import-map.json \\
   --allow-net \\
   https://cdn.skypack.dev/@virtualstate/deno/lib/example.js
 `.trim()}
